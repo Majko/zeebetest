31c453d84cb37a33900deaee24aeb812fffaeda0
Majko <mrnhrnk@gmail.com>
2020-05-20 21:29:22 +0200

    vyseparovana funkcia pre vsetky testy get

--- auth_service/authentication.test.js
+++ auth_service/authentication.test.js
@@ -2,25 +2,35 @@
 const { request } = require('../utils');
 const testjson = JSON.stringify({ meno: 'jozo', priezvisko: 'hrdlo' })
 
-describe('send POST request', () => {
-    test('to non authentcation service', async () => {
-        const headers = {
-            'Content-Type': 'application/json',
-            'Host': 'fe_service_notauth.localhost'
-        }
+const headersNoAuth = {
+    'Content-Type': 'application/json',
+    'Host': 'fe_service_notauth.localhost'
+}
+
+const headersAuth = {
+    'Content-Type': 'application/json',
+    'Host': 'fe_service_auth.localhost',
+    'Authentication': 'iamok'
+}
+
+get = async (headers) => {
+    try {
         const response = await request('http://localhost:80',
             testjson, { headers: headers })
-        expect(response.statusCode = 200)
+        return response
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+describe('send POST request', () => {
+    it('returns status code 2000 to non authentcation service', async () => {
+        const response = await get(headersNoAuth)
+        expect(response.statusCode).toEqual(200);
     })
 
     test('to  authentcation service', async () => {
-        const headers = {
-            'Content-Type': 'application/json',
-            'Host': 'fe_service_auth.localhost',
-            'authentication': 'iamok'
-        }
-        const response = await request('http://localhost:80',
-            testjson, { headers: headers })
-        expect(response.statusCode = 200)
+        const response = await get(headersAuth)
+        expect(response.statusCode).toEqual(200);
     })
 })
