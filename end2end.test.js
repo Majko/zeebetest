ec8ed1fa3f92cb26d8878fcd634b83858b37527d
Majko <mrnhrnk@gmail.com>
2020-05-30 18:52:45 +0200

    Zena lokality na test all services

---
+++ end2end.test.js
@@ -0,0 +1,43 @@
+// do package.json je pre testovanie axios pridat : "jest": { "testEnvironment": "node" }
+const { request } = require('./utils');
+const testjson = JSON.stringify({ meno: 'maros', priezvisko: 'hrdlo' })
+
+retNoAuthHeaders = () => {
+    const headersNoAuth = {
+        'Content-Type': 'application/json',
+        Host: 'fe_service_notauth.localhost'
+    }
+    return headersNoAuth
+}
+
+retAuthHeaders = () => {
+    const headersAuth = {
+        'Content-Type': 'application/json',
+        'Host': 'fe_service_auth.localhost',
+        'Authentication': 'iamok'
+    }
+    return headersAuth
+}
+
+post = async (headers) => {
+    try {
+        const response = await request('http://localhost:80',
+            testjson, { headers: headers })
+        // console.log(response)
+        return response
+    } catch (error) {
+        throw error
+    }
+}
+
+describe('send POST request',  () => {
+    test('returns status code 200 to non authentcation service', async () => {
+        const response = await post(retNoAuthHeaders())
+        expect(response.status).toEqual(200);
+    })
+
+    test('returns status code 200 to authentcation service', async () => {
+        const response = await post(retAuthHeaders())
+        expect(response.status).toEqual(200);
+    })
+})
